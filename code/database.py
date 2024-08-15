b288a42ba65909dd8fa387646e8a76b8e1c665a2
mrmotes <paulthomaslowder@gmail.com>
2024-08-15 13:21:41 -0500

    Updated file organization

--- code/database.py
+++ code/database.py
@@ -2,6 +2,6 @@ from models import Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:////Users/motes/Projects/schoology_assignment_scraper/schoology.db')
+engine = create_engine('sqlite:////Users/motes/Projects/schoology_assignment_scraper/data/schoology.db')
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)

725ef8ea6191bd32840f626e71ebda22b4fdd8b5
p <44508091+mrmotes@users.noreply.github.com>
2025-08-04 14:29:10 -0500

    Update app.py
    specified logging encoding

--- code/app.py
+++ code/app.py
@@ -14,6 +14,7 @@ os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
 
 logging.basicConfig(
     filename=log_file_path,
+    encoding='utf-8',
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
